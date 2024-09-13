df1021b05a6aaaac11b310690a74a3afd81a3751
Harunobu Ishii <harunobuishii547@gmail.com>
2024-09-13 16:32:33 -0400

    error command removed

--- src/tests/c_api/test_join_sail.cpp
+++ src/tests/c_api/test_join_sail.cpp
@@ -13,15 +13,6 @@ using namespace jsoncons;
 
 void download_from_s3(int rank, const std::string& filename) {
     std::string rankStr = std::to_string(rank);
-    
-    // Check if a file exists
-    std::string awsCheck = "aws s3api head-object --bucket secrecy-bucket" + rankStr + " --key " + filename;
-    int check = system(awsCheck.c_str());
-    if (check!=0){
-        std::cout << filename << " does not exist in secrecy-bucket" << rankStr << std::endl;
-        exit(EXIT_FAILURE); 
-    }
-
     std::string awsCommand = "aws s3 cp s3://secrecy-bucket" + rankStr + "/" + filename + " ../";
     int result = system(awsCommand.c_str());
 
